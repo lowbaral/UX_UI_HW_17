aff860e87841d000065e9dfa6742fba75d397f8a
lowbaral <lowbaral@gmail.com>
2021-01-31 16:31:56 -0700

    HW19 Changes
    Added interactions to the pages.

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// console.log("loaded correctly")
+// // $("myID").on("click", function(){
+// //     console.log("you clicked #myID");
+// // });
+// // $( "a.nav-item" ).hover(function() {
+// //       $( this ).append( $( "underline" ) );
+// //     }, 
+// //     function() {
+// //       $( this ).find( "underline" ).last().remove();
+// //     }
+// //   );
+
+// $(".nav-item").linkUnderlineAnim({
+//   // animation speed in millisecond
+//   "speed"       : 300,
+//   // hex color
+//   "color"       :"#DB3340",
+//   // thickness
+//   "thickness"     : 2,
+//   // distance in pixels from top
+//   "distance"      : 0
+// });
